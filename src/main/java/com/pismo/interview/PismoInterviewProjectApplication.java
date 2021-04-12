950f66ea7d3ad726980207adb030596f6d3c8086
Vinícius Martins <viniciusmartinsds@outlook.com>
2021-04-11 21:02:54 -0300

    Initial commit

---
+++ src/main/java/com/pismo/interview/PismoInterviewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pismo.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PismoInterviewProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PismoInterviewProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pismo/interview/PismoInterviewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pismo.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PismoInterviewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
